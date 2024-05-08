f73e1d82b16af1189a5815e18a228c00c515d068
hammer-29 <1132720684@qq.com>
2024-05-08 08:18:56 -0700

    2024年 05月 08日 星期三 08:18:56 PDT

---
+++ note/chapter2/codeup1928.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int month[13]={0,31,29,31,30,31,30,31,31,30,31,30,31};
+bool isleap(int year){
+    if((year%4==0&&year%100!=0)||year%400==0)
+        return true;
+    else
+        return false;
+}
+int main (){
+
+
+
+
+    return 0;
+}
